e824637d76ccebcaab586eafbc21d9af732e76af
lockevod <58392928+lockevod@users.noreply.github.com>
2025-03-29 14:48:08 +0100

    Added custom messages, some fixes

---
+++ app/src/main/kotlin/com/enderthor/kActions/datatype/WebhookDataType.kt
@@ -0,0 +1,13 @@
+package com.enderthor.kActions.datatype
+
+
+import android.content.Context
+
+
+class WebhookDataType(
+    datatype: String,
+    context: Context,
+    value: Int
+) : WebhookDataTypeBase(datatype, context, value)
+
+
